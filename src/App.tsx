523963f2ce230d8040ab2486d2cfcc39c0e4d0c7
franckDev21 <tutodevphp@gmail.com>
2023-02-12 09:40:43 +0100

    add lib: react, react-router, react icons, tailwinds, scss

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="App bg-gray-600">
+      <h1 className='text-green-400'>Init</h1>
+    </div>
+  )
+}
+
+export default App
