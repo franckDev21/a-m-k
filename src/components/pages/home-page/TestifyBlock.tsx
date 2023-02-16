532fec381c93e18762ca75a578daf08048875047
franckDev21 <tutodevphp@gmail.com>
2023-02-16 12:49:46 +0100

    update app

--- src/components/pages/home-page/TestifyBlock.tsx
+++ src/components/pages/home-page/TestifyBlock.tsx
@@ -1,10 +1,57 @@
 import React, { FC } from "react";
 
+
 const TestifyBlock: FC<{ className?: string }> = ({ className = "" }) => {
   return (
-    <h1 className=" mx-auto text-center text-4xl font-extrabold text-transparent  bg-clip-text bg-gradient-to-r to-blue-400 from-primary text-gray-800">
-      Nos étudiants témoignent
-    </h1>
+    <div
+      className={`${className} py-10 h-[900px] relative`}
+      style={{
+        backgroundImage: `linear-gradient(#3C5EAB, #3c5dab57, #3c5dab57),url('/assets/istockphoto-1248897199-1024x1024.jpg')`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="container mx-auto ">
+        <h1 className=" mx-auto mt-5 lg:mt-10 text-center text-4xl font-extrabold text-white">
+          Nos étudiants témoignent
+        </h1>
+
+        <div className="p-5 flex flex-col md:flex-row md:space-x-5 space-y-4 md:space-y-0 w-[99%] md:w-[70%] text-white rounded-md absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="w-[200px] h-[200px] lg:w-[350px] flex-none lg:h-[350px] overflow-hidden relative border-4">
+            <img
+              src="/assets/student1-removebg-preview.png"
+              className="absolute w-full h-auto object-cover"
+            />
+          </div>
+          <div>
+            <p className="text-lg lg:text-xl xl:text-2xl font-light">
+              « La formation Tech Lead d'Adaa est réellement en accord avec le
+              monde de l'entreprise. Il était aussi important que la formation
+              soit au niveau Bac +5, car j'avais déjà un Bac +3 : je ne suis pas
+              déçu du tout, car l’apprentissage est progressif avec l’aide des
+              mentors. »
+            </p>
+            <div className="mt-10">
+              <p className="bg-primary text-white max-w-max px-2">
+                Product Manager, Orange Cameroun
+              </p>
+              <h1 className="text-2xl font-extrabold">Danielle KOTTO</h1>
+              <h1 className="text-tertiary font-bold bg-white max-w-max px-2">
+                Certified PMP
+              </h1>
+            </div>
+          </div>
+          <div className="mt-6 space-x-5 absolute -bottom-28 left-28 hidden lg:flex">
+            <span className="w-4 h-4 rounded-full block bg-white cursor-pointer"></span>
+            <span className="w-4 h-4 rounded-full block bg-white bg-opacity-60 cursor-pointer"></span>
+            <span className="w-4 h-4 rounded-full block bg-white bg-opacity-60 cursor-pointer"></span>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,7 @@ import { BiChevronsLeft, BiChevronsRight } from "react-icons/bi";
 import Accordeons from "../components/pages/home-page/Accordeons";
 import { FaCheck } from "react-icons/fa";
 import NewLetter from "../components/layouts/NewLetter";
+import TestifyBlock from "../components/pages/home-page/TestifyBlock";
 
 const Home = () => {
   return (
@@ -55,48 +56,7 @@ const Home = () => {
         </div>
       </div>
 
-      <div
-        className="py-10 h-[900px] relative"
-        style={{
-          backgroundImage: `linear-gradient(#3C5EAB, #3c5dab57, #3c5dab57),url('/assets/istockphoto-1248897199-1024x1024.jpg')`,
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "top center",
-          backgroundSize: "cover",
-        }}
-      >
-        <div className="container mx-auto ">
-          <h1 className=" mx-auto mt-5 lg:mt-10 text-center text-4xl font-extrabold text-white">
-            Nos étudiants témoignent
-          </h1>
-
-          <div className="p-5 flex flex-col md:flex-row md:space-x-5 space-y-4 md:space-y-0 w-[99%] md:w-[70%] text-white rounded-md absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-            <div className="w-[200px] h-[200px] lg:w-[350px] flex-none lg:h-[350px] overflow-hidden relative border-4">
-              <img
-                src="/assets/student1-removebg-preview.png"
-                className="absolute w-full h-auto object-cover"
-              />
-            </div>
-            <div>
-              <p className="text-lg lg:text-xl xl:text-2xl font-light">
-                « La formation Tech Lead d'Adaa est réellement en accord avec le
-                monde de l'entreprise. Il était aussi important que la formation
-                soit au niveau Bac +5, car j'avais déjà un Bac +3 : je ne suis
-                pas déçu du tout, car l’apprentissage est progressif avec l’aide
-                des mentors. »
-              </p>
-              <div className="mt-10">
-                <p className="bg-primary text-white max-w-max px-2">
-                  Product Manager, Orange Cameroun
-                </p>
-                <h1 className="text-2xl font-extrabold">Danielle KOTTO</h1>
-                <h1 className="text-tertiary font-bold bg-white max-w-max px-2">
-                  Certified PMP
-                </h1>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <TestifyBlock />
 
       <div className="container mx-auto pb-10 pt-16">
         <h1 className="w-[79%] mx-auto text-center text-2xl lg:text-4xl font-extrabold text-transparent  bg-clip-text bg-gradient-to-r to-blue-400 from-primary text-gray-800">
